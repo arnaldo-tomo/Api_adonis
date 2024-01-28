068bfda5785b0fa1115db6ff330cd5aab3e282be
Arnaldo tomo <arnaldotomo@gmail.com>
2024-01-28 17:50:56 +0200

    Update 1706364757960_create_users_table.ts

--- database/migrations/1706364757960_create_users_table.ts
+++ database/migrations/1706364757960_create_users_table.ts
@@ -6,10 +6,10 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
+      // table.bigint('pessoa_id').notNullable().unsigned().references('id').inTable('pessoas')
       table.string('full_name').nullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
-
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
     })
