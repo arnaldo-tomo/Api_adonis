f3b8cbe9858f4fa4bae6d73b7db654a7b42441dc
Arnaldo tomo <arnaldotomo@gmail.com>
2024-01-28 17:51:00 +0200

    Update 1706365585995_create_pessoas_table.ts

--- database/migrations/1706365585995_create_pessoas_table.ts
+++ database/migrations/1706365585995_create_pessoas_table.ts
@@ -6,6 +6,7 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.integer('user_id').notNullable().unsigned().references('id').inTable('users')
       table.string('nome')
       table.string('sexo')
       table.string('descricao')
