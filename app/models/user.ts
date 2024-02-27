9fa01bd1ffafca657190079918dd3e742f6c6efe
Arnaldo tomo <arnaldotomo@gmail.com>
2024-02-27 12:51:42 +0200

    Update user.ts

--- app/models/user.ts
+++ app/models/user.ts
@@ -1,11 +1,12 @@
-// eslint - disable prettier / prettier
-import Pessoa from './pessoa'
+
 import { DateTime } from 'luxon'
 import { withAuthFinder } from '@adonisjs/auth'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { BaseModel, belongsTo, column, hasOne } from '@adonisjs/lucid/orm'
-// import { BelongsTo } from '@adonisjs/lucid/types/relations';
+import Profile from './profile.js'
+import { BelongsTo } from '@adonisjs/lucid/types/relations'
+
 
 const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
   uids: ['email'],
@@ -13,9 +14,13 @@ const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
 })
 
 export default class User extends compose(BaseModel, AuthFinder) {
-  @hasOne(() => Pessoa)
-  public pessoa: HasOne<typeof Pessoa>
 
+  profile() {
+    return this.hasOne('App/Models/Profile')
+  }
+  hasOne(arg0: string) {
+    throw new Error('Method not implemented.')
+  }
   @column({ isPrimary: true })
   declare id: number
 
@@ -28,10 +33,19 @@ export default class User extends compose(BaseModel, AuthFinder) {
   @column()
   declare password: string
 
+  @column()
+  declare ProfileId: number
+
+  @column()
+  declare pessoasId: number
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @belongsTo(() => Profile)
+  public profile: BelongsTo<typeof Profile>
 
 }
