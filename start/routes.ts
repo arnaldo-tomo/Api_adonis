c3981653ec17fd10854e79fda8b3819dd9e1950e
Arnaldo tomo <arnaldotomo@gmail.com>
2024-02-29 15:58:24 +0200

    Update routes.ts

--- start/routes.ts
+++ start/routes.ts
@@ -2,8 +2,8 @@ import router from '@adonisjs/core/services/router'
 const ControllerMainsController = () => import('#controllers/controller_mains_controller')
 const PessoasController = () => import('#controllers/pessoas_controller')
 
+router.get('/', [PessoasController, 'show'])
 router.post('index', [PessoasController, 'index'])
-router.get('show', [PessoasController, 'show'])
 router.post('destroy', [PessoasController, 'destroy'])
 router.post('updade', [PessoasController, 'updade'])
 
